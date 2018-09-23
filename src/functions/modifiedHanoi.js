8b10434033614bb31514c0f9394826a2b5c0096c
Roman Borodatov <roman.borodatov@gmail.com>
2018-09-23 16:24:47 +0300

    colorSwap function created

--- src/functions/modifiedHanoi.js
+++ src/functions/modifiedHanoi.js
@@ -1,19 +1,4 @@
-const universalHanoi = (data) => {
-	let arr = data.split(" ");
-	let vitaminsArray = arr.map((vitamin) => {
-		return {
-			name: vitamin.split("")[0],
-			color:  vitamin.split("")[1],
-			prevColor: ""
-		}
-	});
-
-	const isWhite = (vitamin) => {
-		if (vitamin.color === "W"){
-			return true;
-		}
-	};
-
+const canBeSwaped = (vitamin, destColor, arr) => {
 	const isMaxi = (vitamin, arr) => {
 		let color = vitamin.color;
 		let num = vitamin.name
@@ -31,7 +16,7 @@ const universalHanoi = (data) => {
 		return true
 	}
 
-	const willBeMaxi = ((vitamin, destColor, arr) => {
+	const willBeMaxi = (vitamin, destColor, arr) => {
 		let num = vitamin.name;
 
 		let toCheck = arr.filter((item) => {
@@ -45,11 +30,42 @@ const universalHanoi = (data) => {
 			return false
 		}
 		return true
+	};
+
+	return (isMaxi(vitamin, arr) && willBeMaxi(vitamin, destColor, arr))
+};
+
+const colorSwap = (vitamin, destColor, arr, movesLog=[]) => {
+	if (canBeSwaped(vitamin, destColor, arr) && destColor != vitamin.prevColor) {
+		const newArr = arr.map((item) => {
+			if (item.name === vitamin.name) {
+				item = { ...item, prevColor: item.color, color: destColor};
+			}
+			return item;
+		});
+		movesLog.push([vitamin.name, vitamin.color, destColor]);
+
+		return { arr: newArr, movesLog };
+	}
+}
+
+const universalHanoi = (data) => {
+	let arr = data.split(" ");
+	let vitaminsArray = arr.map((vitamin) => {
+		return {
+			name: vitamin.split("")[0],
+			color:  vitamin.split("")[1],
+			prevColor: ""
+		}
 	});
 
-	const canBeSwaped = ((vitamin, destColor, arr) => {
-		return (isMaxi(vitamin, arr) && willBeMaxi(vitamin, destColor, arr))
-	});
+	const isWhite = (vitamin) => {
+		if (vitamin.color === "W"){
+			return true;
+		}
+	};
+	
+	console.log(colorSwap(vitaminsArray[2], 'B', vitaminsArray));	
 };
 
 universalHanoi("3B 4B 5W");
